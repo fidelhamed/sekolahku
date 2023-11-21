57762581dfa56bf44dad998325c1cc52d39b4d60
fidelhamed <85061053+fidelhamed@users.noreply.github.com>
2023-11-22 01:05:25 +0700

    Update index.blade.php

--- Modules/PPDB/Resources/views/backend/dataMurid/index.blade.php
+++ Modules/PPDB/Resources/views/backend/dataMurid/index.blade.php
@@ -61,9 +61,9 @@
                                                     <td> {{$key+1}} </td>
                                                     <td> {{$murids->name}} </td>
                                                     <td> {{$murids->email}} </td>
-                                                    <td> {{$murids->muridDetail->proses}} </td>
+                                                    <td> {{$murids->muridDetail->proses == 'Terverifikasi' ? 'Lulus Admininstrasi' : 'Pendaftaran'}} </td>
                                                     <td> {{$murids->paymentRegis->status}} </td>
-                                                    <td> {{$murids->role}} </td>
+                                                    <td> {{$murids->role == 'Terverifikasi' ? 'Calon Murid' : 'Guest'}} </td>
                                                     <td>
                                                         <a href="{{route('data-murid.show', $murids->id)}}" class="btn btn-info btn-sm" >Detail</a>
                                                         <a href="" class="btn btn-success btn-sm" style="display: {{ $murids->role !== 'Terverifikasi' ? 'none' : '' }}">Lulus</a>
